2666e6547248dd2fecedcdc3577ec41552df94b3
Betoniarka <d.grudzinski.96@gmail.com>
2025-10-26 14:48:12 +0100

    Very basic FastAPI and HTML response

--- app/horizons.py
+++ app/horizons.py
@@ -1,23 +1,16 @@
+
+'''
+Horizons API provides access to key solar system data and production of ephemerides for solar system objects:
+asteroids, comets, planetary satellites, 8 planets (+ Pluto), the Sun, L1, L2, select spacecraft, and system barycenters.
+'''
+
 import httpx
 import urllib
 import re
 from dotenv import load_dotenv
 from os import environ as env
 
-
-load_dotenv()
-
-
 URL = "https://ssd.jpl.nasa.gov/api/horizons.api"
-
-# OBSERVER LOCATION
-E_LON = env["E_LON"] #degrees
-LAT = env["LAT"] #degrees
-ALT = env["ALT"] #km, with respect to the reference ellipsoid, not “mean sea level”. convert location to alt: https://geographiclib.sourceforge.io/cgi-bin/GeoidEval
-
-# TIME RANGE
-START_TIME = "2025-10-25 18:00" #YYYY-MM-DD HH:MM:SS.fff
-STOP_TIME = "2025-10-26 09:00" #YYYY-MM-DD HH:MM:SS.fff
 
 # SOLAR SYSTEM OBJECTS
 SOLAR_SYSTEM = {
@@ -33,40 +26,69 @@ SOLAR_SYSTEM = {
     "Pluto": 999
 }
 
-OBJECT_TO_SEARCH = SOLAR_SYSTEM["Uranus"]
+def request_data(
+        object: str = "Uranus",
+        obj_data: str = "NO",
+        make_ephem: str = "YES",
+        site_coord: str = "18.55,53.09,0.029", #Torun Observatory
+        start_time: str = "2025-10-25 18:00",
+        stop_time: str = "2025-10-26 18:00",
+        step_size: str = "1 h",
+        time_zone: str = "+01:00"
+        ):
+    
+    '''Request data about Object from NASA HORIZONS API.
+    
+    Data is in huge text file, even if JSON format is requested. It has to be parsed using (name of the function).
+    
+    Args:
+    - object: e.g. Mars
+    - obj_data: NO/YES, for object data, e.g. mass
+    - make_ephem: NO/YES, for ephemerides of object
+    - site_coord: coordinates of observer on Earth, "e-lon,lat,alt(not msl)"
+    - start_time: beginning of ephem calc
+    - stop_time: end of ephem calc
+    - step_size: step time of ephem calc
+    - time_zone: time zone
+    '''
 
-# REQUEST PARAMS
-params = {
+    params = {
     "format": "json",
-    "COMMAND": f"'{OBJECT_TO_SEARCH}'", #799 is Uranus
-    "OBJ_DATA": "'NO'",
-    "MAKE_EPHEM": "'YES'",
+    "COMMAND": f"'{SOLAR_SYSTEM[object]}'", #799 is Uranus
+    "OBJ_DATA": f"'{obj_data}'", #NO/YES
+    "MAKE_EPHEM": f"'{make_ephem}'", #NO/YES
     "EPHEM_TYPE": "'OBSERVER'",
     "CENTER": "'coord'",
-    "SITE_COORD": f"'{E_LON},{LAT},{ALT}'",
-    "START_TIME": f"'{START_TIME}'",
-    "STOP_TIME": f"'{STOP_TIME}'",
-    "STEP_SIZE": "'1 h'",
+    "SITE_COORD": f"'{site_coord}'", #e.g. 18.55,53.09,0.029 - e-lon, lat, alt (not msl)
+    "START_TIME": f"'{start_time}'", #YYYY-MM-DD HH:MM:SS.fff
+    "STOP_TIME": f"'{stop_time}'", #YYYY-MM-DD HH:MM:SS.fff
+    "STEP_SIZE": f"'{step_size}'",
     "QUANTITIES": "'2,4'",
-    "TIME_ZONE": "'+02:00'"
-}
+    "TIME_ZONE": f"'{time_zone}'"
+    }
 
-encoded_params = urllib.parse.urlencode(params, quote_via=urllib.parse.quote, safe=",@'")
+    encoded_params = urllib.parse.urlencode(params, quote_via=urllib.parse.quote, safe=",@'")
 
-result = {}
+    request = httpx.get(f"{URL}?{encoded_params}")
+    response = request.json()
 
-def download_data():
-    global result
-    r = httpx.get(f"{URL}?{encoded_params}")
-    result = r.json()
-    return
+    parsed = re.search(r"\$\$SOE(.*?)\$\$EOE", response["result"], re.DOTALL)
+    planet = re.search(r"Target body name:(.*?)\(", response["result"])
 
-download_data()
+    return response, planet.group(1), parsed.group(1), 
 
-parsed = re.search(r"\$\$SOE(.*?)\$\$EOE", result["result"], re.DOTALL)
-planet = re.search(r"Target body name:(.*?)\(", result["result"])
 
-print(result["signature"])
-print("")
-print(planet.group(1))
-print(parsed.group(1))
+if __name__ == "__main__":
+
+    load_dotenv()
+
+    # OBSERVER LOCATION, USED IF HORIZONS.PY IS LAUNCHED AS SINGULAR FILE
+    E_LON = env["E_LON"] #degrees
+    LAT = env["LAT"] #degrees
+    ALT = env["ALT"] #km, with respect to the reference ellipsoid, not “mean sea level”. convert location to alt: https://geographiclib.sourceforge.io/cgi-bin/GeoidEval
+
+    data = request_data(object="Mars", site_coord=f"{E_LON},{LAT},{ALT}")
+
+    print(data[1])
+    print(data[2])
+    input("Enter to close...")
--- app/main.py
+++ app/main.py
@@ -1,1 +1,44 @@
-# fastapi
+from datetime import datetime
+import time
+import horizons
+from dotenv import load_dotenv
+from os import environ as env
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from enum import Enum
+
+
+load_dotenv()
+
+# OBSERVER LOCATION
+E_LON = env["E_LON"] #degrees
+LAT = env["LAT"] #degrees
+ALT = env["ALT"] #km, with respect to the reference ellipsoid, not “mean sea level”. convert location to alt: https://geographiclib.sourceforge.io/cgi-bin/GeoidEval
+
+class SolarObjects(Enum):
+    sun = "Sun"
+    mercury = "Mercury"
+    venus = "Venus"
+    mars = "Mars"
+    jupiter = "Jupiter"
+    saturn = "Saturn"
+    uranus = "Uranus"
+    neptune = "Neptune"
+    pluto = "Pluto"
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"msg": "Root page"}
+
+@app.get("/solar/{obj}", response_class=HTMLResponse)
+def get_data(obj: SolarObjects):
+    data = horizons.request_data(object=obj.value, site_coord=f"{E_LON},{LAT},{ALT}")
+    formatted_data = str(data[2]).replace("\n", "<br>")
+    return f"<h2>{obj.value}</h2><pre>{formatted_data}</pre>"
+
+
+
+# print(datetime.now())
+# print(time.localtime().tm_gmtoff)
